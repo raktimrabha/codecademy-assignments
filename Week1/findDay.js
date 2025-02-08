9944a4bd3aea65d469c06a3734ee0669ada00b36
rktm <rabharaktim@outlook.com>
2025-02-09 01:16:49 +0530

    Add all conditions, change filename etc.
    fix issues, filename,

---
+++ Week1/findDay.js
@@ -0,0 +1,41 @@
+/* 
+ program that determines the name of the day in a week,
+ based on a number input from the user. 
+ Using a switch-case statement to implement the logic.
+*/
+
+const prompt = require("prompt-sync")();
+
+// display title
+console.log("\n- - - Find name of the day - - -\n");
+
+// takes user input in string; converts it to number (+)
+let userInput = +prompt("Enter number between 1 and 7: ");
+
+// checks input and returns log
+switch (userInput) {
+    case 1:
+        console.log(` ${userInput} : The day is Monday\n`);
+        break;
+    case 2:
+        console.log(`${userInput} : The day is Tuesday\n`)
+        break;
+    case 3:
+        console.log(`${userInput} : The day is Wednesday\n`)
+        break;
+    case 4:
+        console.log(`${userInput} : The day is Thursday\n`)
+        break;
+    case 5:
+        console.log(`${userInput} : The day is Friday\n`)
+        break
+    case 6:
+        console.log(`${userInput} : The day is Saturday\n`)
+        break;
+    case 7:
+        console.log(`${userInput} : The day is Sunday\n`)
+        break;
+    default:
+        console.log("Invalid input. Please enter a number between 1 and 7.")
+        break;
+}
--- Week1/switch-assign01.js
+++
@@ -1,5 +0,0 @@
-// switch case to take input: 
-
-console.log("Week 1 - Switch Assignment 01");
-
-for ()
