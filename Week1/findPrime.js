e531b61515c3bec05ecf72297263cd8e404cd163
rktm <rabharaktim@outlook.com>
2025-02-15 16:53:41 +0530

    fix find prime logic
    fix 1 is not prime

--- Week1/findPrime.js
+++ Week1/findPrime.js
@@ -4,24 +4,26 @@ const prompt = require("prompt-sync")();
 
 let number = +prompt("Enter a number: ");
 
-if (isNaN(number)) {
-    console.log("Invalid input. Please  enter  a valid  number.")
+if (isNaN(number) || number < 1) {
+    console.log("Invalid input. Please  enter  a valid  number.");
 } else {
-
     let isPrime = true;
-    if (number < 0) {
-        false;
+
+    //   check for prime number
+    if (number === 1) {
+        isPrime = false; // 1 is not a prime number
     } else {
         for (let i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i === 0) {
+            if (number % i === 0) { // if a number is divisible by 2; set not prime
                 isPrime = false;
                 break;
             }
         }
     }
+
     if (isPrime) {
-        console.log(`The number ${number} is a prime number.`)
+        console.log(`The number ${number} is a prime number.`);
     } else {
-        console.log(`The number ${number} is not a prime number.`)
+        console.log(`The number ${number} is not a prime number.`);
     }
 }
